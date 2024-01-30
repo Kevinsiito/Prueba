328aee089edbeee5c330222575d37f5027e8319e
Kevinsiito <kevin.lararangel16@gmail.com>
2024-01-30 11:52:29 -0600

    primer problema del examen diagnostico resuelto

--- clases.js
+++ clases.js
@@ -29,11 +29,24 @@ class Categoria{
     let informacion = `ID: ${this.id}, Nombre: ${this.nombre}`;
 
     if (this.subcategorias.length > 0) {
-      informacion += `\nSubcategorías: ${this.subcategorias.join(', ')}`;
+      informacion += `\nSubcategorías: ${this.subcategorias.map(subcat => subcat.nombre).join(', ')}`;
     }
 
     return informacion;
   }
 }
 
+const categoriaPrincipal = new Categoria(1, 'Electrónicos');
+const subcategoria1 = new Categoria(2, 'Smartphones');
+const subcategoria2 = new Categoria(3, 'Laptops');
 
+categoriaPrincipal.agregarSubcategoria(subcategoria1);
+categoriaPrincipal.agregarSubcategoria(subcategoria2);
+
+const producto1 = new Producto(101, 'Laptop HP', 'Potente laptop con procesador i7', 1200.0);
+
+const im1 = new Imagen(1, 'iphone14_imagenOficial.jpg');
+
+console.log(categoriaPrincipal.obtenerInformacion());
+console.log(`Producto: ${producto1.producto}, Descripción: ${producto1.descripcion}, Precio: ${producto1.precio}`);
+console.log(`Imagen: ${im1.id}, URL: ${im1.url}`);
